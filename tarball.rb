af11fc5459d757ad2bcc37bb4c82956eb20774a8
Christian Fritsch <christian.fritsch@burda.com>
2022-08-14 13:36:40 +0200

    first commit

---
+++ tarball.rb
@@ -0,0 +1,43 @@
+require "capistrano/scm/plugin"
+
+# By convention, Capistrano plugins are placed in the
+# Capistrano namespace. This is completely optional.
+module Capistrano
+  class TarballPlugin < ::Capistrano::SCM::Plugin
+
+    def set_defaults
+      # Define any variables needed to configure the plugin.
+      set_if_empty :archive_path, "archive.tar.gz"
+    end
+
+    def define_tasks
+      # The namespace can be whatever you want, but its best
+      # to choose a name that matches your plugin name.
+      namespace :tarball do
+        task :set_current_revision do
+          set :current_revision, "25"
+        end
+
+        task :create_release do
+          # Your code to create the release directory and copy
+          # the source code into it goes here.
+          on release_roles :all do
+            execute :mkdir, "-p", release_path
+            upload! fetch(:archive_path), '/tmp'
+            @archive_name = File.basename(fetch(:archive_path))
+            execute :tar, "-xzf", "/tmp/#{@archive_name}", "-C", release_path
+            execute :rm, "/tmp/#{@archive_name}"
+          end
+        end
+      end
+    end
+
+    def register_hooks
+      # Tell Capistrano to run the custom create_release task
+      # during deploy.
+      after "deploy:new_release_path", "tarball:create_release"
+      before "deploy:set_current_revision", "tarball:set_current_revision"
+    end
+
+  end
+end
